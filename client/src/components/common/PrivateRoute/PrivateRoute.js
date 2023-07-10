69b24a10c58c5f829a3fbb364ab8abf04ae2b5d1
Nevyan Plamenov <plamenovnevyan@gmail.com>
2023-07-10 13:22:21 +0300

    Fixed a breaking bug with private route

--- client/src/components/common/PrivateRoute/PrivateRoute.js
+++ client/src/components/common/PrivateRoute/PrivateRoute.js
@@ -4,7 +4,7 @@ import { useLocalStorage } from "../../../hooks/useLocalStorage"
 export const PrivateRoute = () => {
   const {storedData} = useLocalStorage()
 
-  if(!storedData.session){
+  if(Object.values(storedData).length <= 0){
     return <Navigate  to={"/"} replace/>
   }
   else {
